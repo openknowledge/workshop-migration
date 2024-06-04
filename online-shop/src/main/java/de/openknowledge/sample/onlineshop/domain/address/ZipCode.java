d216580359ad85098e5e8c648b4dde3f2f41a73d
Arne Limburg <arne.limburg@openknowledge.de>
2024-06-04 12:34:38 +0200

    Implement online shop monolith

---
+++ online-shop/src/main/java/de/openknowledge/sample/onlineshop/domain/address/ZipCode.java
@@ -0,0 +1,39 @@
+/*
+ * Copyright (C) open knowledge GmbH.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package de.openknowledge.sample.onlineshop.domain.address;
+
+import static java.util.Objects.requireNonNull;
+import static org.apache.commons.lang3.Validate.matchesPattern;
+
+import javax.json.bind.annotation.JsonbTypeAdapter;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+
+import de.openknowledge.sample.onlineshop.domain.address.ZipCode.ZipCodeAdapter;
+import de.openknowledge.sample.onlineshop.infrastructure.jsonb.AbstractRecordValueTypeAdapter;
+import jakarta.persistence.Embeddable;
+
+@Embeddable
+@JsonbTypeAdapter(ZipCodeAdapter.class)
+public record ZipCode(@NotNull @Pattern(regexp = "\\d{5}") String code) {
+    public ZipCode {
+        requireNonNull(code);
+        matchesPattern(code, "\\d{5}");
+    }
+
+    public static class ZipCodeAdapter extends AbstractRecordValueTypeAdapter<ZipCode, String> {
+    }
+}
